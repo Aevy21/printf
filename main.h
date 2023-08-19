507bdb6238be146f0babe96b99b92d718d1bdcb4
Avumile Ndlovu <avumileee@gmail.com>
2023-08-19 16:13:58 -0700

    a function that handles s , c, % format specifiers

---
+++ main.h
@@ -0,0 +1,20 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+int puts(const char *string);
+
+int _printf(const char *format, ...);
+
+
+
+
+
+
+#endif 
+
