58607a23ecdc13e105160f80dc04cdc22a31dfa1
Avumile Ndlovu <avumileee@gmail.com>
2023-08-21 08:37:04 -0700

    fun.c betty error

--- fun.c
+++ fun.c
@@ -2,9 +2,10 @@
 
 /**
  * print_percent - Print a percent symbol ('%')
+ *@fmt_specs:a va_list containing a string to print.
  * Return: 1 (the number of characters printed).
  */
-int print_percent(va_list __attribute__ ((unused)) fmt_specs)
+int print_percent(va_list __attribute__ ((unused))fmt_specs)
 {
 	_putchar('%');
 	return (1);
