606986e23f1b912f723c8e3d1d904c8e2186e2b2
S1995l <skhosanasl95@gmail.com>
2023-08-21 02:06:53 -0700

    functions

---
+++ fun.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_percent - Print a percent symbol ('%')
+ * Return: 1 (the number of characters printed).
+ */
+int print_percent(void)
+{
+	_putchar('%');
+	return (1);
+}
+
