01aa525ea09ad44073e7e5af3703c8db18711199
S1995l <skhosanasl95@gmail.com>
2023-08-22 15:03:58 -0700

    octal.c

--- octal.c
+++ octal.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
-* print_octal - unsigned int argument is converted to unsigned octal
-* @fmt_specs: unsigned to be converted
-* Descriptions: prints unsigned octal
-* Return: size the output
-*/
+ * print_octal - unsigned int argument is converted to unsigned octal
+ * @fmt_specs: unsigned to be converted
+ * Descriptions: prints unsigned octal
+ * Return: size the output
+ */
 int print_octal(va_list fmt_specs)
 {
 	unsigned int len, base_eit, j, digit, n, num;
 	int count = 0;
+
 	n = va_arg(fmt_specs, unsigned int);
+
 	if (n != 0)
 	{
 		num = n;
@@ -20,6 +22,7 @@ int print_octal(va_list fmt_specs)
 			len++;
 		}
 		base_eit = 1;
+
 		for (j = 1; j <= len - 1; j++)
 			base_eit *= 8;
 		for (j = 1; j <= len; j++)
